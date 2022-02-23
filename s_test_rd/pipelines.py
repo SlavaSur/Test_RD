d80bf505b5d60a3677ec187fb8fd930eff1619f4
SlavaSur <vjacheslav.surovcev@gmail.com>
2022-02-23 14:11:19 +0300

    Pipeline uodate data to MongoD

--- s_test_rd/pipelines.py
+++ s_test_rd/pipelines.py
@@ -6,8 +6,16 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymongo
 
+class STestRdPipeline(object):
+    def __init__(self):
+        self.conn =pymongo.MongoClient('localhost', 27017)
+        db = self.conn['test_rd']
+        self.collection = db['rd']
+      
 
-class STestRdPipeline:
     def process_item(self, item, spider):
+        self.collection.insert_one(item)
+#        self.collection.insert(item)
         return item
--- s_test_rd/spiders/spider_test.py
+++ s_test_rd/spiders/spider_test.py
@@ -1,34 +1,42 @@
 import scrapy
-#from s_test_rd.items import STestRdItem
+from s_test_rd.items import STestRdItem
 
 class RDSpider(scrapy.Spider):
     name = "spider_test"
+    #print('Введите ссылки:')
+    #start_urls = input().split(', ')
     start_urls = ['https://github.com/scrapy', 'https://github.com/celery/']
 
-
+    #https://github.com/scrapy, https://github.com/celery/
     def parse(self, response):
         yield response.follow(response.css('a.UnderlineNav-item ::attr(href)').getall()[1], callback=self.parse)
         for link in response.xpath('//a[@class="d-inline-block"]/@href').getall():
             yield response.follow(link, callback=self.parse_info)
+            #yield response.follow(response.xpath('//a[@class="Link--primary d-flex no-underline"]/@href').get(), callback=self.parse_info)
 
 
     def parse_info(self, response):
-        yield {
-            'name_rep': response.xpath('//a[@data-pjax="#repo-content-pjax-container"]/text()').extract_first(),
-            'about': response.xpath('//p[@class="f4 my-3"]/text()').get().strip(),
-            'link': response.xpath('//a[@class="text-bold"]/@href').get(),
-            'stars': response.xpath('//span[@class="Counter js-social-count"]/@title').get(),
-            'forks': response.xpath('//span[@class="Counter"]/@title').get(),
-            'watching': response.xpath('//div[@class="mt-2"]//strong/text()').extract()[1],
-            'commits': response.xpath('//span[@class="d-none d-sm-inline"]//strong/text()').get(),
-            'info_last_commit': {'author_commit': response.xpath('//a[@class="commit-author user-mention"]/text()').get(),
-                                 'title_commit': response.xpath('//a[@class="Link--primary markdown-title"]/@title').get(),
-                                 'datetime_commit': response.xpath('//relative-time[@class="no-wrap"]/@datetime').get()},
-            'release': response.xpath('//a[@class="Link--primary no-underline"]//span/@title').get(),
-            'info_last_release': {'version': response.xpath('//span[@class="css-truncate css-truncate-target text-bold mr-2"]/text()').get(),
-                                  'datetime_release': response.xpath('//div[@class="text-small color-fg-muted"]//relative-time[@class="no-wrap"]/@datetime').get()}
-            # #l_3['changelog'] = ????????
+        item = STestRdItem()
+        item_1 = STestRdItem()
+        item_2 = STestRdItem()
+        item['name_rep'] = response.xpath('//a[@data-pjax="#repo-content-pjax-container"]/text()').extract_first()
+        item['about'] = response.xpath('//p[@class="f4 my-3"]/text()').get()
+        item['link'] = response.xpath('//a[@class="text-bold"]/@href').get()
+        item['stars'] = response.xpath('//span[@class="Counter js-social-count"]/@title').get()
+        item['forks'] = response.xpath('//span[@class="Counter"]/@title').get()
+        item['watching'] = response.xpath('//div[@class="mt-2"]//strong/text()').extract()[1]
+        item['commits'] = response.xpath('//span[@class="d-none d-sm-inline"]//strong/text()').get()
+        item_1['author_commit'] = response.xpath('//a[@class="commit-author user-mention"]/text()').get()
+        item_1['title_commit'] = response.xpath('//a[@class="Link--primary markdown-title"]/@title').get()
+        item_1['datetime_commit'] = response.xpath('//relative-time[@class="no-wrap"]/@datetime').get()
+        item['info_last_commit'] = item_1
+        item['release'] = response.xpath('//a[@class="Link--primary no-underline"]//span/@title').get()
+        item_2['version'] = response.xpath('//span[@class="css-truncate css-truncate-target text-bold mr-2"]/text()').get()
+        item_2['datetime_release'] = response.xpath('//div[@class="text-small color-fg-muted"]//relative-time[@class="no-wrap"]/@datetime').get()
+        item_2['changelog']: 'changelog'
+        item['info_last_release'] = item_2
+        yield item
+          #response.xpath('//div[@class="markdown-body my-3"]/p').getall()}
 
-        }
 
 
